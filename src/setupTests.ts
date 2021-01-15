efc874bee26819787e75802fa7c324e2b75e0f5d
Andy Meek <andy@andymeek.co.uk>
2021-01-15 17:42:34 +0000

    test: add jest canvas  mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,4 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
 import MutationObserver from '@sheerun/mutationobserver-shim'
+import 'jest-canvas-mock'
 window.MutationObserver = MutationObserver
